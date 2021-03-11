64dbc458c74e68430961c3a1fa7cff71e6b39302
Sean Patterson <thesnellvillian@gmail.com>
2021-03-10 20:31:12 -0500

    Hi

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
-require('./models/User');
-require('./models/Survey');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 const keys = require('./config/keys');
 const bodyParser = require('body-parser');
-
+require('./models/User');
+require('./models/Survey');
 require('./services/passport');
 const app = express();
 
+mongooose.Promise = global.Promise;
 mongoose.connect(keys.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true} );
 
 app.use(bodyParser.json());
@@ -29,15 +28,18 @@ require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 require('./routes/surveyRoutes')(app);
 
-if (process.env.NODE_ENV === 'production'){
+if (process.env.NODE_ENV === 'production') {
+    // Express will serve up production assets
+    // like our main.js file, or main.css file!
     app.use(express.static('client/build'));
-    const path = require('path'); 
-
+  
+    // Express will serve up the index.html file
+    // if it doesn't recognize the route
+    const path = require('path');
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
-}
-
+  }
 
 const PORT = process.env.PORT || 5000; 
 app.listen(PORT);
